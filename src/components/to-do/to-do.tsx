246e1b0983136f9f6d6d7f1c0a2669efd8440d0e
emmafattori <emmalfattori@gmail.com>
2025-01-09 20:25:34 -0500

    Adding function to caption adding todo

--- src/components/to-do/to-do.tsx
+++ src/components/to-do/to-do.tsx
@@ -4,13 +4,31 @@ export interface ToDoProps {
     title: string;
     description: string;
     completed: boolean;
-    dueDate: Date;
 }
 
 export const Todo = () => {
 
     const [todos, setTodos] = useState<ToDoProps[]>([]);
+
+const addTodo = (title: string, description: string) => {
+        const newTodo: ToDoProps = {
+            title,
+            description,
+            completed: false,
+        }
+        setTodos([...todos, newTodo]);
+    }
     return (
-        <section className="shadow-xl w-1/2 p-4 rounded-xl">This is the todo widget</section>
+        <section className="shadow-xl w-1/2 p-4 rounded-xl">
+
+
+            <div>
+                <h2>Daily To Dos</h2>
+                <form>
+                    <input type="text" placeholder="Get groceries..." />
+                    <button onSubmit={() => addTodo('test', 'test')}>Add</button>
+                </form>
+            </div>
+        </section>
     )
 }
